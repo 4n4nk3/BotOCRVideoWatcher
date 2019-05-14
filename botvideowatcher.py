fe9428dbe4c209ceac4019ecb46fc39f688cfddb
4n4nk3 <47717886+4n4nk3@users.noreply.github.com>
2019-05-14 11:18:34 +0200

    Create botvideowatcher.py

---
+++ botvideowatcher.py
@@ -0,0 +1,23 @@
+from my_clicker import *
+from my_ocr import *
+import mss
+import mss.tools
+from time import sleep
+
+while 1:
+    sleep(5)
+    with mss.mss() as sct:
+        # The screen part to capture
+        monitor = {"top": 1011, "left": 2651, "width": 34, "height": 14}
+        output = "immagine.png"
+
+        # Grab the data
+        sct_img = sct.grab(monitor)
+
+        # Save to the picture file
+        mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+        print(output)
+        timer = ocr_scan(output)
+        print(timer)
+        sleep(timer)
+        avanti()
